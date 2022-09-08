8ef7594d0c6291b4a6a87b2fbd25326af6a4aa2b
Ayush-13-02 <ayushsharma13jun@gmail.com>
2022-09-08 13:13:31 +0530

    Second commit

--- Onlinelibrary/settings.py
+++ Onlinelibrary/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-c!$=r=@2uaoni3)x2zy8%vq+klf7s1xf-sqx2wi$r6@qp5sqcv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['127.0.0.1.8000','softlib.herokuapp.com/']
 
 
 # Application definition
