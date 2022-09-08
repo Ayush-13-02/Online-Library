933cff98ac39fba8a81b38805b9362ad145a0734
Ayush-13-02 <ayushsharma13jun@gmail.com>
2022-09-08 12:11:21 +0530

    Second commit

--- Onlinelibrary/urls.py
+++ Onlinelibrary/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-# from django.conf import settings
-# from django.conf.urls import static
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("__reload__/", include("django_browser_reload.urls")),
     path('', include('softLib.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
