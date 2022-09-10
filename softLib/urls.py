a3be5f19a2513524878cc57b323c97615b14543a
Ayush-13-02 <ayushsharma13jun@gmail.com>
2022-09-10 23:52:54 +0530

    commit

--- softLib/urls.py
+++ softLib/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home',views.Home, name="Home")
+    path('home',views.Home, name="Home"),
+    path('other',views.Other, name="Other")
 ]
--- softLib/views.py
+++ softLib/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 #HomePage
 def Home(request):
     return render(request,'Home.html')
+
+def Other(request):
+    return render(request,'Books.html')
