82cb59a0b3701ca21ad1b0342bfc3e0440698064
Ayush-13-02 <ayushsharma13jun@gmail.com>
2022-09-21 21:57:01 +0530

    updates

--- Onlinelibrary/urls.py
+++ Onlinelibrary/urls.py
@@ -20,5 +20,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("__reload__/", include("django_browser_reload.urls")),
-    path('', include('softLib.urls')),
+    path("", include('softLib.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- softLib/urls.py
+++ softLib/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.Home, name="Home"),
+    path('/',views.Home, name="Home"),
     path('login',views.Login, name="Login")
 ]
