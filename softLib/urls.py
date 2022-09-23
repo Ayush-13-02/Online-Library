db24575bf761a12e8979c7ad1b6ad585cb65689c
Ayush-13-02 <ayushsharma13jun@gmail.com>
2022-09-23 18:54:53 +0530

    updates

--- softLib/urls.py
+++ softLib/urls.py
@@ -3,6 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('/',views.Home, name="Home"),
-    path('login',views.Login, name="Login")
+    path('',views.Home, name="Home"),
+    path('profile',views.Profile, name="Profile"),
+    path('login',views.Login, name="Login"),
+    path('logout',views.Logout, name="Logout"),
+    path('register',views.Register, name="Register")
 ]
--- softLib/views.py
+++ softLib/views.py
@@ -1,9 +1,55 @@
 from django.http import HttpResponse
-from django.shortcuts import render
-
+from django.shortcuts import redirect, render
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate,login,logout
+from django.contrib import messages
 #HomePage
 def Home(request):
     return render(request,'Books.html')
 
+def Profile(request):
+    return render(request,'Profile.html')
 def Login(request):
-    return render(request,'Login.html')
+    print("Ayush Sharma",request.method)
+    if request.method == 'POST':
+        loginusernameame = request.POST['email']
+        loginpassword = request.POST['pass']
+        print(loginusernameame,loginpassword)
+        user = authenticate(username = loginusernameame, password = loginpassword)
+        print("Hello, ",user)
+        if user is not None:
+            login(request,user)
+            messages.success(request,"Successfully logged In")
+            return redirect('/')
+        else:
+            messages.error("Email or Password is wrong")
+            return redirect('login')
+    else:
+        return render(request,'Login.html')
+
+def Register(request):
+    if request.method == 'POST':
+        fname = request.POST['fname']
+        lname = request.POST['lname']
+        email = request.POST['email']
+        username = email
+        pass1 = request.POST['pass1']
+        pass2 = request.POST['pass2']
+
+        if pass1 == pass2:
+            myuser = User.objects.create_user(username,email,pass1)
+            myuser.first_name = fname
+            myuser.last_name = lname
+            myuser.save()
+            messages.success(request, "Register in EBook Library Successfully")
+            return redirect('/')
+        else:
+            HttpResponse(request,"Passward doesn't match")
+            return redirect('/register')
+    else:
+        return render(request,'Register.html')
+
+def Logout(request):
+    logout(request)
+    messages.success(request, "Logged out Successfully")
+    return redirect('/')
