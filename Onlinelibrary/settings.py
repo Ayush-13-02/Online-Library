909da025867a1a808303d8bd66f0229245c3356b
Ayush-13-02 <ayushsharma13jun@gmail.com>
2022-09-26 22:40:04 +0530

    updates

--- Onlinelibrary/settings.py
+++ Onlinelibrary/settings.py
@@ -122,10 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'softLib/static')
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'static')
-]
 MEDIA_ROOT = os.path.join(BASE_DIR,'')
 MEDIA_URL = '/media/'
 # Default primary key field type
