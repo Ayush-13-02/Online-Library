85662edeb1a33a98ca6808cad6f65b24139e2f87
Ayush-13-02 <ayushsharma13jun@gmail.com>
2022-10-26 20:59:52 +0530

    deploy

--- Onlinelibrary/settings.py
+++ Onlinelibrary/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ['SECURITY_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["softbooklib.herokuapp.com","localhost"]
+ALLOWED_HOSTS = ["softlib.herokuapp.com","localhost"]
 
 
 # Application definition
