0ed6822166b0162e594365272743684a36b558b6
Ayush-13-02 <ayushsharma13jun@gmail.com>
2022-11-08 22:19:52 +0530

    Add with backend

--- Onlinelibrary/settings.py
+++ Onlinelibrary/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -138,3 +139,5 @@ INTERNAL_IPS = [
     "127.0.0.1",
 ]
 NPM_BIN_PATH = r"C:\Program Files\nodejs\npm.cmd"
+
+django_heroku.settings(locals())
